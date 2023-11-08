87743c512699a43c222ecfb006ede2fc1e6861da
PrinceGeord <lukekidwell@live.co.uk>
2023-11-08 14:04:52 +0000

    film-page ready for review

--- front-end/src/app/services/tmdb-api.service.ts
+++ front-end/src/app/services/tmdb-api.service.ts
@@ -15,7 +15,7 @@ baseURL: 'https://api.themoviedb.org/3/search'
   getFilmByTitle(title: string){
     return this.api.get(`movie?query=${title}&include_adult=false&language=en-US&page=1`, {
       headers: {
-        Authorization: '' // Requires API Key input - removed before pushing to GitHub
+        Authorization: 'bearer ' // Requires API Key input - removed before pushing to GitHub
       }
     })
   }
