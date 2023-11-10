38cda7026f16a25caaf87bb0044de208a2559265
Billy White <billywhite@Billys-Mac-mini.local>
2023-11-10 15:06:08 +0000

    home carosel injected and css more installed

--- front-end/src/app/components/search-results-page/search-results-page.component.ts
+++ front-end/src/app/components/search-results-page/search-results-page.component.ts
@@ -8,46 +8,43 @@ import { identifierName } from '@angular/compiler';
 @Component({
   selector: 'app-search-results-page',
   templateUrl: './search-results-page.component.html',
-  styleUrls: ['./search-results-page.component.css']
+  styleUrls: ['./search-results-page.component.css'],
 })
 export class SearchResultsPageComponent {
   filmCards: FilmCardable[] = [];
 
-
   constructor(
     private tmdbApiService: TmdbApiService,
-    private route: ActivatedRoute,
-    ) {}
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.getFilmQuery()
-
-}
-
-// getFilm() {this.tmdbApiService
-//       .getFilmByTitle(this.film.title)
-//       .subscribe((filmCards) => (this.filmCards = filmCards));}
-
-
-      getFilmQuery(): void {
-        const rawQuery = this.route.snapshot.queryParams
-        const queryKey = Object.keys(rawQuery)[0]           // to be used later when search query functionality is built out to include other criteria e.g. search by genre, actor etc
-        const queryValue = Object.values(rawQuery)[0]
-        console.log(queryValue)
-        this.tmdbApiService.getFilmByTitle(queryValue).then(({data: {results}}) => {
-          console.log(results)
-          results.forEach((result: any, index: number) => {
-          const filmCard = {id: result.id,
-          title: result.title,
-        img: `https://image.tmdb.org/t/p/w500${result.poster_path}`,
-        avgRating: 4,
-        friendReviews: ["barbara,Harry"]}
-            this.filmCards.push(filmCard)
-          })
-          
-
-        })
-      }
-
-
+    this.getFilmQuery();
   }
+
+  // getFilm() {this.tmdbApiService
+  //       .getFilmByTitle(this.film.title)
+  //       .subscribe((filmCards) => (this.filmCards = filmCards));}
+
+  getFilmQuery(): void {
+    const rawQuery = this.route.snapshot.queryParams;
+    const queryKey = Object.keys(rawQuery)[0]; // to be used later when search query functionality is built out to include other criteria e.g. search by genre, actor etc
+    const queryValue = Object.values(rawQuery)[0];
+    console.log(queryValue);
+    this.tmdbApiService
+      .getFilmByTitle(queryValue)
+      .then(({ data: { results } }) => {
+        console.log(results);
+        results.forEach((result: any, index: number) => {
+          const filmCard = {
+            id: result.id,
+            title: result.title,
+            img: `https://image.tmdb.org/t/p/w500${result.poster_path}`,
+            avgRating: 4,
+            friendReviews: ['barbara,Harry'],
+          };
+          this.filmCards.push(filmCard);
+        });
+      });
+  }
+}
