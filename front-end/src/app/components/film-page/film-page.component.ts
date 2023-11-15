a202047aa55c64f8b90787a0f697e034d3f81e18
PrinceGeord <lukekidwell@live.co.uk>
2023-11-15 11:21:09 +0000

    refactor of film-page to allow for ratings to come through also added logo

--- front-end/src/app/components/film-page/film-page.component.ts
+++ front-end/src/app/components/film-page/film-page.component.ts
@@ -41,40 +41,48 @@ export class FilmPageComponent {
     return director[0]['name'];
   }
   // Accessing our back-end
-  getFilmDetails(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.tmdbApiService
-      .getFilmById(id)
-      .then(({ data }) => {
-        const nullImage = 'assets/image-not-found.png';
-        let imageSource;
+  async getFilmDetails() {
+    try {
+      const id = Number(this.route.snapshot.paramMap.get('id'));
+      const { data } = await this.tmdbApiService.getFilmById(id);
+      let rating: any = 3;
+      try {
+        const rating = await this.tmdbApiService.getFilmRatingById(id);
+      } catch {
+        rating = 3;
+      }
+      const nullImage = 'assets/image-not-found.png';
+      let imageSource;
 
-        if (data.poster_path === null) {
-          imageSource = 'assets/image-not-found.png';
-        } else {
-          imageSource = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
-        }
-        this.synopsisCard = {
-          title: data.title,
-          image: imageSource,
-          year: data.release_date.slice(0, 4),
-          overview: data.overview,
-          language: data.original_language,
-          avgRating: 4,
-          runtime: data.runtime,
-          genre: this.getGenres(data.genres).join(', '),
-          director: this.getDirector(data.credits.crew),
-          actors: [
-            data.credits.cast[0].name,
-            data.credits.cast[1].name,
-            data.credits.cast[2].name,
-            data.credits.cast[3].name,
-          ].join(', '),
-        };
-        this.isLoaded = true;
-      })
-      .catch((error) => {
-        console.error('Error', error);
-      });
+      console.log(rating.data);
+      if (data.poster_path === null) {
+        imageSource = 'assets/image-not-found.png';
+      } else {
+        imageSource = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+      }
+
+      rating.toFixed(2);
+
+      this.synopsisCard = {
+        title: data.title,
+        image: imageSource,
+        year: data.release_date.slice(0, 4),
+        overview: data.overview,
+        language: data.original_language,
+        avgRating: rating,
+        runtime: data.runtime,
+        genre: this.getGenres(data.genres).join(', '),
+        director: this.getDirector(data.credits.crew),
+        actors: [
+          data.credits.cast[0].name,
+          data.credits.cast[1].name,
+          data.credits.cast[2].name,
+          data.credits.cast[3].name,
+        ].join(', '),
+      };
+      this.isLoaded = true;
+    } catch (error) {
+      console.error('Error', error);
+    }
   }
 }
